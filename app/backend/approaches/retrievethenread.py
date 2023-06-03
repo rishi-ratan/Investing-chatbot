52a0ed6dc163e41083def5ef1534d8f82fff86e8
Rishi Ratan <ratanrishi@gmail.com>
2023-06-03 14:08:54 +0000

    Change approach description for investment research to use in an intelligent assistant.

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -10,7 +10,7 @@ from text import nonewlines
 class RetrieveThenReadApproach(Approach):
 
     template = \
-"You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. " + \
+"You are an intelligent assistant helping investment analyst retrieve information from investment books, annual reports, and earnings transcripts of public and private companies." + \
 "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
 "Answer the following question using only the data provided in the sources below. " + \
 "For tabular information return it as an html table. Do not return markdown format. "  + \
